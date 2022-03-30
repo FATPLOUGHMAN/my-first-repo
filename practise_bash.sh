2f6c8cc2f8281f3d2bf7b8b74e0f7732dcc15d80
FATPLOUGHMAN <102633298+FATPLOUGHMAN@users.noreply.github.com>
2022-03-30 17:00:22 +1100

    Update practise_bash.sh

--- practise_bash.sh
+++ practise_bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # practise parsing some html, remove tags and print everything on newlines
-html=$(curl 'https://www.linkedin.com/in/john-lozano-b50806/')
-echo $html | sed 's/<[^>]*>//g' | tr ' ' '\n'
+html=$(curl 'https://www.linkedin.com/in/sarahblake44/')
+echo $html | sed 's/<[^>]*>//g' | tr ' ' '\n' | sed "/^$/d"
